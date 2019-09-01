d0e1b8759f24a143061d5949c84bbbeefaef60f2
yurikrupnik <krupnik.yuri@gmail.com>
2019-09-01 14:17:37 +0300

    init

---
+++ packages/p1/__tests__/p1.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const p1 = require('..');
+
+describe('p1', () => {
+    it('needs tests');
+});
---
+++ packages/p2/__tests__/p2.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const p2 = require('..');
+
+describe('p2', () => {
+    it('needs tests');
+});
